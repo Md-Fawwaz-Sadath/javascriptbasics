5777f414ad6b79be285cde67e4b6d0bf738e28a3
mdfawwaz03 <mdfawwaz03@gmail.com>
2024-10-23 17:50:43 +0000

    added data types, variable name rules

---
+++ DataTypesDemo.js
@@ -0,0 +1,6 @@
+let productPrice = 1500000;
+let productDiscount = 15;
+let productName = "Samsung Galaxy S22 Ultra";
+let productColor = "Black";
+let isAvailable = true;
+let productRating = 4.9;
---
+++ variableName.js
@@ -0,0 +1,8 @@
+let first_name = "Mohammed";
+let last_name = " Ali";
+console.log(first_name + last_name);
+let carrier = "Web Developer";
+let age = 23;
+console.log(carrier + " " + age);
+
+
