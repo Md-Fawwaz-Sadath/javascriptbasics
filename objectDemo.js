b6d9f2b57c0085e59080481c72744a9fec34ed57
mdfawwaz03 <mdfawwaz03@gmail.com>
2024-10-25 13:29:51 +0000

    added new demo

---
+++ objectDemo.js
@@ -0,0 +1,16 @@
+let product = {
+    name: "Samsung S22 ultra",
+    brand: "Samsung",
+    color: "black",
+    ram: 8,
+    rom: 256,
+    price: 1500000,
+    discount: 10,
+    isAvailabe: true,
+    
+};
+console.log(product);
+console.log(product.name);
+console.log(product.price);
+console.log(product.discount);
+console.log(product.isAvailabe);
--- variableName.js
+++ variableName.js
@@ -4,5 +4,19 @@ console.log(first_name + last_name);
 let carrier = "Web Developer";
 let age = 23;
 console.log(carrier + " " + age);
+let minusZero = -0;
+console.log(minusZero);
+let isAvailabe = true;
+console.log(isAvailabe);
+let x;
+console.log(typeof x);
+let y = null;
+console.log(typeof y);
+let z;
+console.log(typeof z);
+
+let veryBigNum = 1234567890123456789068968907608976807680768996897678967896978697n;
+console.log(veryBigNum);
+
 
 
