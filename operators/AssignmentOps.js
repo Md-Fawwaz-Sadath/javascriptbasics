4b83fbe1857543399e3d59e2fb27a67aef958af1
mdfawwaz03 <mdfawwaz03@gmail.com>
2024-10-25 14:07:32 +0000

    added demos

--- operators/AssignmentOps.js
+++ operators/AssignmentOps.js
@@ -8,3 +8,11 @@ console.log(x /= 5);
 console.log(x %= 5);
 console.log(x **= 5);
 console.log(x);
+
+console.log(Math.floor(10/3));
+console.log(Math.ceil(10/3));
+console.log(Math.round(10/3));
+console.log(Math.sqrt(25));
+console.log(Math.pow(2, 3));
+console.log(Math.min(1, 2, 3, 4, 5));
+console.log(Math.max(1, 2, 3, 4, 5));
